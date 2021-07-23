f09a76c98be32359fa296610ba57c7cbcd4f7e92
@nder1996 <72629491+nder1996@users.noreply.github.com>
2021-07-23 16:08:43 -0500

    Pagina_Cliente

---
+++ js/Login.js
@@ -0,0 +1,19 @@
+
+function Login(){
+
+	var Email = $("#Input_Email").val();
+
+
+	var Password = $("#Input_Password").val();
+
+	if (Email=="ander@gmail.com"){
+		if(Password=="123456789"){
+
+			//window.open('http://ejemplo.com/archivo.pdf', '_blank');
+
+			window.open('Cliente.html')
+		
+		}
+	}
+
+}
